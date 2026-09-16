ed22a43080e3d8a39a28e4f1b68f95fb2e9d6143
Waqas Ahmed <wahmed3900@gmail.com>
2026-09-15 22:31:28 -0700

    disable debug logging

--- auth.ts
+++ auth.ts
@@ -9,6 +9,5 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     }),
   ],
   secret: process.env.AUTH_SECRET ?? process.env.NEXTAUTH_SECRET,
-  trustHost: true,
-  debug: true,
+  
 });
