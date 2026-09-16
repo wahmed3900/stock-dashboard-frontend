5b6d63498840f3dee16c28bc5804f04ac98ffbb2
Waqas Ahmed <wahmed3900@gmail.com>
2026-09-15 17:27:58 -0700

    enable nextauth debug logging

--- auth.ts
+++ auth.ts
@@ -1,7 +1,20 @@
 import NextAuth from "next-auth";
+ import Google from "next-auth/providers/goimport NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET ?? process.env.NEXTAUTH_SECRET,
+  trustHost: true,
+  debug: true,
+});ogle";
+
+√export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
